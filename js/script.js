5c6363d3b6906684acb645b93bc024c4f9318e6d
wintermute28 <a28c@yandex.ru>
2023-10-02 16:56:33 +0300

    Add sending forms

--- js/script.js
+++ js/script.js
@@ -245,6 +245,59 @@ window.addEventListener("DOMContentLoaded", () => {
 		".menu .container"
 	).render();
 	
+	//Forms
 
+	const forms = document.querySelectorAll("form");
+
+	const message = {
+		loading: "загрузка",
+		success: "Спасибо! Скоро мы с Вами свяжемся!",
+		fail: "Что-то пошло не так..."
+	};
+
+	forms.forEach(item => {
+		postData(item);
+	});
+
+	function postData(form) {
+		form.addEventListener("submit", (e) => {
+			e.preventDefault();
+
+			let statusMessage = document.createElement("div");
+			statusMessage.classList.add("status");
+			statusMessage.textContent = message.loading;
+			form.appendChild(statusMessage);
+
+			const request = new XMLHttpRequest();
+			request.open("POST", "server.php");
+
+			request.setRequestHeader("Content-type", "aplication/json");
+			// при использовании FormData с XMLHttpRequest заголовок указывать НЕ НУЖНО!!!
+			const formData = new FormData(form);
+
+			const object = {};
+			formData.forEach(function(value, key) {
+				object.key = value;
+			});//создаем объект из FormData и далее уже его переформатируем в JSON
+
+			const json = JSON.stringify(object);
+
+
+			request.send(json);
+
+			request.addEventListener("load", () => {
+				if (request.status === 200) {
+					console.log(request.response);
+					statusMessage.textContent = message.success;
+					form.reset();
+					setTimeout(() => {
+						statusMessage.remove();
+					}, 2000);
+				} else {
+					statusMessage.textContent = message.fail;
+				}
+			});
+		});
+	}
 
 });
