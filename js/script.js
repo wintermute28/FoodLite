b535fc82258439f15d3345cbfa215cd8070e53c3
wintermute28 <a28c@yandex.ru>
2023-10-11 16:08:30 +0300

    Add nav dots for slider

--- js/script.js
+++ js/script.js
@@ -355,6 +355,7 @@ window.addEventListener("DOMContentLoaded", () => {
 	// Slider
 
 	const slides = document.querySelectorAll(".offer__slide"),
+		  slider = document.querySelector(".offer__slider"),
 		  prev =document.querySelector(".offer__slider-prev"),
 		  next = document.querySelector(".offer__slider-next"),
 		  total = document.querySelector("#total"),
@@ -366,6 +367,7 @@ window.addEventListener("DOMContentLoaded", () => {
 	let slideIndex = 1;
 	let offset = 0;
 
+	// Второй вариант слайдера - карусель
 
 	if(slides.length < 10) {
 		total.textContent = `0${slides.length}`;
@@ -384,6 +386,50 @@ window.addEventListener("DOMContentLoaded", () => {
 	slides.forEach(slide => {
 		slide.style.width = width;
 	});
+
+	slider.style.position = "relative";
+
+	const indicators = document.createElement("ol"),
+		  dots = [];
+	indicators.classList.add("carousel-indicators");
+	indicators.style.cssText = `
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: 15;
+		display: flex;
+		justify-content: center;
+		margin-right: 15%;
+		margin-left: 15%;
+		list-style: none;
+	`;
+	slider.append(indicators);
+
+	for (let i = 0; i < slides.length; i++) {
+		const dot = document.createElement("li");
+		dot.setAttribute("data-slide-to", i + 1);
+		dot.style.cssText = `
+			box-sizing: content-box;
+			flex: 0 1 auto;
+			width: 30px;
+			height: 6px;
+			margin-right: 3px;
+			margin-left: 3px;
+			cursor: pointer;
+			background-color: #fff;
+			background-clip: padding-box;
+			border-top: 10px solid transparent;
+			border-bottom: 10px solid transparent;
+			opacity: .5;
+			transition: opacity .6s ease;
+		`;
+		if (i == 0) {
+			dot.style.opacity = 1;
+		}
+		indicators.append(dot);
+		dots.push(dot);
+	}
 
 	next.addEventListener("click", () => {
 		if(offset == +width.slice(0, width.length - 2) * (slides.length -1)) {
@@ -405,6 +451,9 @@ window.addEventListener("DOMContentLoaded", () => {
 		} else {
 			current.textContent = slideIndex;
 		}
+
+		dots.forEach(dot => dot.style.opacity = ".5");
+		dots[slideIndex - 1].style.opacity = 1;
 	});
 
 	prev.addEventListener("click", () => {
@@ -429,6 +478,29 @@ window.addEventListener("DOMContentLoaded", () => {
 		} else {
 			current.textContent = slideIndex;
 		}
+
+		dots.forEach(dot => dot.style.opacity = ".5");
+		dots[slideIndex - 1].style.opacity = 1;
+	});
+
+	dots.forEach(dot => {
+		dot.addEventListener("click", (e) => {
+			const slideTo = e.target.getAttribute("data-slide-to");
+
+			slideIndex = slideTo;
+			offset = +width.slice(0, width.length - 2) * (slideTo -1);
+
+			slidesField.style.transform = `translateX(-${offset}px)`;
+
+			if (slides.length < 10) {
+				current.textContent = `0${slideIndex}`;
+			} else {
+				current.textContent = slideIndex;
+			}
+
+			dots.forEach(dot => dot.style.opacity = ".5");
+			dots[slideIndex - 1].style.opacity = 1;
+		});
 	});
 
 
@@ -474,7 +546,7 @@ window.addEventListener("DOMContentLoaded", () => {
 	// });
 
 
-	// Второй вариант слайдера - карусель
+
 
 
 
