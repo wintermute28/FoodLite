454c17dc97e71643f6b00268a272e52fa3966b6a
wintermute28 <a28c@yandex.ru>
2023-10-16 17:40:11 +0300

    Use Webpack

---
+++ js/modules/cards.js
@@ -0,0 +1,103 @@
+function cards() {
+	// Используем классы для карточек
+
+	class MenuCard {
+		constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.descr = descr;
+			this.price = price;
+			this.classes = classes;
+			this.parent = document.querySelector(parentSelector);
+			this.transfer = 97;
+			this.changeToRUB();
+		}
+
+		changeToRUB() {
+			this.price = this.price * this.transfer;
+		}
+
+		render() {
+			const element = document.createElement("div");
+
+			if (this.classes.length === 0) {
+				this.element = "menu__item";
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach(className => element.classList.add(className));
+			}
+
+			element.innerHTML = `
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> руб./день</div>
+				</div>
+			`;
+			this.parent.append(element);
+		}
+	}
+
+
+	const getResource = async (url, data) => {
+		const res = await fetch(url);
+
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status ${res.status}`);
+		}
+
+		return await res.json();
+	};
+
+	// Первый вариант:
+	// getResource("http://localhost:3000/menu")
+	// 	.then(data => {
+	// 		data.forEach(({img, altimg, title, descr, price}) => {
+	// 			new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+	// 		});
+	// 	});
+
+	//Еше 3-ий вариант с использованием библиотеки axios
+	axios.get("http://localhost:3000/menu")
+		.then(data => {
+			data.data.forEach(({img, altimg, title, descr, price}) => {
+			 			new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+			});
+		});
+
+
+
+	// 2-ой вариант формирования верстки(без использования классов)
+	// getResource("http://localhost:3000/menu")
+	// 	.then(data => createCard(data));
+	
+	// function createCard(data) {
+	// 	data.forEach(({img, altimg, title, descr, price}) => {
+	// 		const element = document.createElement("div");
+
+	// 		element.classList.add("menu__item");
+			
+	// 		element.innerHTML = `
+	// 			<img src=${img} alt=${altimg}>
+	// 			<h3 class="menu__item-subtitle">${title}</h3>
+	// 			<div class="menu__item-descr">${descr}</div>
+	// 			<div class="menu__item-divider"></div>
+	// 			<div class="menu__item-price">
+	// 				<div class="menu__item-cost">Цена:</div>
+	// 				<div class="menu__item-total"><span>${price}</span> руб./день</div>
+	// 			</div>
+	// 		`;
+
+	// 		document.querySelector(".menu .container").append(element);
+	// 	});
+	// }
+
+
+	
+}
+
+module.exports = cards;
---
+++ js/modules/modal.js
@@ -0,0 +1,65 @@
+function modal() {
+	//Modal
+
+	const modalTrigger = document.querySelectorAll("[data-modal]"),
+		modal = document.querySelector(".modal");
+
+
+	function openModal() {
+		//первый способ
+		modal.classList.add("show");
+		modal.classList.remove("hide");
+		//второй способ
+		// modal.classList.toggle("show");
+
+		document.body.style.overflow = "hidden";//убираем прокрутку страницы при открытом модальном окне
+		clearInterval(modalTimerId);//отменяет ооткрытие окна по таймеру, если пользователь сам его уже открывал ранее
+	}
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener("click", openModal);
+	});
+
+	function closeModal() {
+		//первый способ
+		modal.classList.add("hide");
+		modal.classList.remove("show");
+		//второй способ
+		// modal.classList.toggle("show");
+ 
+		document.body.style.overflow = "";//восстанавливаем прокрутку при закрытии модального
+	}
+
+
+	// modalCloseBtn.addEventListener("click", closeModal);
+	//модальное окно закрывается при клике крестик
+
+	modal.addEventListener("click", (e) => {
+		if (e.target === modal || e.target.getAttribute("data-close") == "") {
+			closeModal();
+		}//модальное окно закрывается при клике на подложку или крестик
+	});
+
+	document.addEventListener("keydown", (e) => {
+		if (e.code === "Escape" && modal.classList.contains("show")) {
+			closeModal();
+		}//модальное окно закрывается при клике на 'Esc'
+	});
+
+	const modalTimerId = setTimeout(openModal, 50000);
+
+	function showModalByScroll() {
+		if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight -0.5) {
+			//Модальное окно появляется при прокрутки страницы до конца
+			openModal();
+			window.removeEventListener("scroll", showModalByScroll);
+			//после одного раза событие удаляется
+
+		}
+
+	}
+
+	window.addEventListener("scroll", showModalByScroll);
+}
+
+module.exports = modal;
